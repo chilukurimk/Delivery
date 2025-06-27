56c5cb321e68a144c10dd589c5a2722bb8accaa9
Madhu Kiran Chilukuri <104894376+chilukurimk@users.noreply.github.com>
2025-06-27 18:45:50 +0000

    Added sample data and updated restaurants from data input

--- main.py
+++ main.py
@@ -1,30 +1,48 @@
-
+import json
 from fastapi import FastAPI
 from pydantic import BaseModel
 from typing import Optional,List
 
 app = FastAPI()
 
-
 class ItemModel(BaseModel):
     id: int
     name: str
-    price: float
-    description: str = None
+    price: int
+    description: Optional[str] = None
+
+
+class ItemsResponse(BaseModel):
+    item_list: List[ItemModel]
+
+
+@app.get("/items")
+def items() -> ItemsResponse:
+    item_list = [
+        ItemModel(id=1, name="Veg Biryani", price=180, description="Spicy and flavorful"),
+        ItemModel(id=2, name="Paneer Tikka", price=220, description="Grilled cottage cheese"),
+        ItemModel(id=3, name="Butter Naan", price=40)
+    ]
+    return ItemsResponse(item_list=item_list)
+
+
 
 class RestaurantModel(BaseModel):
     id: int
     name: str
     location: str
 
+
 class RestaurantsResponse(BaseModel):
     rest_list: List[RestaurantModel]
 
+
 @app.get("/restaurants", response_model=RestaurantsResponse)
 def restaurants() -> RestaurantsResponse:
-    rest_list = [
-        RestaurantModel(id=1, name="Spice Villa", location="Banjara Hills"),
-        RestaurantModel(id=2, name="Grill & Chill", location="Madhapur"),
-        RestaurantModel(id=3, name="Saffron Bite", location="Kukatpally")
-    ]
-    return RestaurantsResponse(rest_list = rest_list)
+
+    with open("sample.json", "r") as file:
+        data = json.load(file)
+    rest_list = []
+    for restaurant in data["rest_list"]:
+        rest_list.append(RestaurantModel(id = restaurant["id"], name = restaurant["name"], location= restaurant["location"]))
+    return RestaurantsResponse(rest_list= rest_list)
